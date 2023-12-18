1cb39cbd51c804505e1df114262d88ec6dbfcc9f
Ana Lobato <93808443+AnaCarolinaLobato@users.noreply.github.com>
2023-12-18 20:22:29 +1300

    Update accelerometer_landing.sql

--- accelerometer_landing.sql
+++ accelerometer_landing.sql
@@ -1,18 +1,10 @@
-CREATE EXTERNAL TABLE `accelerometer_landing`(
-  `user` string COMMENT 'from deserializer',
-  `timestamp` bigint COMMENT 'from deserializer',
-  `x` float COMMENT 'from deserializer',
-  `y` float COMMENT 'from deserializer',
-  `z` float COMMENT 'from deserializer')
-ROW FORMAT SERDE
-  'org.openx.data.jsonserde.JsonSerDe'
-WITH SERDEPROPERTIES (
-  'paths'='timeStamp,user,x,y,z')
-STORED AS INPUTFORMAT
-  'org.apache.hadoop.mapred.TextInputFormat'
-OUTPUTFORMAT
-  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION
-  's3://...'
-TBLPROPERTIES (
-  'classification'='json')
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`accelerometer_landing` (
+  `user` string,
+  `x` float,
+  `y` float,
+  `z` float
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://lake-house-stedi/accelerometer_landing/'
+TBLPROPERTIES ('classification' = 'json');
