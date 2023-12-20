3b71d0a1b8f35b1301aebc0d7e08e1a902731534
Ana Lobato <93808443+AnaCarolinaLobato@users.noreply.github.com>
2023-12-20 21:57:15 +1300

    Update accelerometer _trusted.py

--- accelerometer _trusted.py
+++ accelerometer _trusted.py
@@ -12,63 +12,47 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node accelerometer Landing
-accelerometerLanding_node1702885895743 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://lake-house-stedi/accelerometer_landing/"],
-        "recurse": True,
-    },
-    transformation_ctx="accelerometerLanding_node1702885895743",
-)
-
-# Script generated for node Customer Trusted
-CustomerTrusted_node1702885393040 = glueContext.create_dynamic_frame.from_catalog(
+# Script generated for node accelerometer_landing
+accelerometer_landing_node1703061474056 = glueContext.create_dynamic_frame.from_catalog(
     database="stedi",
-    table_name="customer_landing",
-    transformation_ctx="CustomerTrusted_node1702885393040",
+    table_name="accelerometer_landing",
+    transformation_ctx="accelerometer_landing_node1703061474056",
 )
 
-# Script generated for node Privacy Join
-PrivacyJoin_node1702886701554 = Join.apply(
-    frame1=CustomerTrusted_node1702885393040,
-    frame2=accelerometerLanding_node1702885895743,
-    keys1=["email"],
-    keys2=["user"],
-    transformation_ctx="PrivacyJoin_node1702886701554",
+# Script generated for node customer_trusted
+customer_trusted_node1703061620875 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="customer_trusted",
+    transformation_ctx="customer_trusted_node1703061620875",
+)
+
+# Script generated for node customer privacy join
+customerprivacyjoin_node1703061569715 = Join.apply(
+    frame1=accelerometer_landing_node1703061474056,
+    frame2=customer_trusted_node1703061620875,
+    keys1=["user"],
+    keys2=["email"],
+    transformation_ctx="customerprivacyjoin_node1703061569715",
 )
 
 # Script generated for node Drop Fields
-DropFields_node1702886774518 = DropFields.apply(
-    frame=PrivacyJoin_node1702886701554,
-    paths=[
-        "serialnumber",
-        "birthday",
-        "registrationdate",
-        "sharewithresearchasofdate",
-        "customername",
-        "sharewithfriendsasofdate",
-        "email",
-        "lastupdatedate",
-        "phone",
-        "sharewithpublicasofdate",
-    ],
-    transformation_ctx="DropFields_node1702886774518",
+DropFields_node1703061893643 = DropFields.apply(
+    frame=customerprivacyjoin_node1703061569715,
+    paths=["email", "phone"],
+    transformation_ctx="DropFields_node1703061893643",
 )
 
-# Script generated for node Accelerometer Trusted
-AccelerometerTrusted_node1702886906761 = glueContext.write_dynamic_frame.from_options(
-    frame=DropFields_node1702886774518,
+# Script generated for node Amazon S3
+AmazonS3_node1703062189919 = glueContext.write_dynamic_frame.from_options(
+    frame=DropFields_node1703061893643,
     connection_type="s3",
     format="json",
     connection_options={
-        "path": "s3://lake-house-stedi/accelerometer_landing/",
-        "compression": "snappy",
+        "path": "s3://lake-house-stedi/accelerometer_landing/trusted/",
         "partitionKeys": [],
     },
-    transformation_ctx="AccelerometerTrusted_node1702886906761",
+    transformation_ctx="AmazonS3_node1703062189919",
 )
 
 job.commit()
+
