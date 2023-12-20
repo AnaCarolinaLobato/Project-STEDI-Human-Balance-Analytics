cd14d64c9b849c49028616e93f476e6bf962dc36
Ana Lobato <93808443+AnaCarolinaLobato@users.noreply.github.com>
2023-12-20 21:01:28 +1300

    Update accelerometer_landing.sql

--- accelerometer_landing.sql
+++ accelerometer_landing.sql
@@ -1,10 +1,22 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`accelerometer_landing` (
-  `user` string,
-  `x` float,
-  `y` float,
-  `z` float
-)
-ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://lake-house-stedi/accelerometer_landing/'
-TBLPROPERTIES ('classification' = 'json');
+CREATE EXTERNAL TABLE `accelerometer_landing`(
+  `user` string COMMENT 'from deserializer', 
+  `timestamp` bigint COMMENT 'from deserializer', 
+  `x` float COMMENT 'from deserializer', 
+  `y` float COMMENT 'from deserializer', 
+  `z` float COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'case.insensitive'='TRUE', 
+  'dots.in.keys'='FALSE', 
+  'ignore.malformed.json'='FALSE', 
+  'mapping'='TRUE') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://lake-house-stedi/accelerometer_landing'
+TBLPROPERTIES (
+  'classification'='json', 
+  'transient_lastDdlTime'='1702898387')
