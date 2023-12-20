e90ec92b75a4a9ddd0dc8708a4d79ea72f6375c6
Ana Lobato <93808443+AnaCarolinaLobato@users.noreply.github.com>
2023-12-21 01:17:10 +1300

    Create step_trainer_landing.sql

---
+++ step_trainer_landing.sql
@@ -0,0 +1,20 @@
+CREATE EXTERNAL TABLE `step_trainer_landing`(
+  `sensorreadingtime` bigint COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `distancefromobject` bigint COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'case.insensitive'='TRUE', 
+  'dots.in.keys'='FALSE', 
+  'ignore.malformed.json'='FALSE', 
+  'mapping'='TRUE') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://lake-house-stedi/step_trainer_landing'
+TBLPROPERTIES (
+  'classification'='json', 
+  'transient_lastDdlTime'='1703068346')
