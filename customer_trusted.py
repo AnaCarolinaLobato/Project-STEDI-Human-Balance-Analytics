3a7bc9114cf2ed96c45ad88be4be9808182c9601
Ana Lobato <93808443+AnaCarolinaLobato@users.noreply.github.com>
2023-12-22 21:24:43 +1300

    Update customer_trusted.py

--- customer_trusted.py
+++ customer_trusted.py
@@ -4,10 +4,11 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-import gs_null_rows
-import re
 
+# Get job arguments
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+
+# Initialize GlueContext, SparkContext, and Job
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
@@ -15,7 +16,7 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 # Script generated for node Amazon S3
-AmazonS3_node1702898838528 = glueContext.create_dynamic_frame.from_options(
+s3_source_frame = glueContext.create_dynamic_frame.from_options(
     format_options={"multiline": False},
     connection_type="s3",
     format="json",
@@ -23,31 +24,28 @@ AmazonS3_node1702898838528 = glueContext.create_dynamic_frame.from_options(
         "paths": ["s3://lake-house-stedi/customer_landing/"],
         "recurse": True,
     },
-    transformation_ctx="AmazonS3_node1702898838528",
-)
-
-# Script generated for node Remove Null Rows
-RemoveNullRows_node1702900843084 = AmazonS3_node1702898838528.gs_null_rows(
-    extended=True
+    transformation_ctx="s3_source_frame",
 )
 
 # Script generated for node Filter
-Filter_node1702900946384 = Filter.apply(
-    frame=RemoveNullRows_node1702900843084,
-    f=lambda row: (not (row["shareWithResearchAsOfDate"] == 0)),
-    transformation_ctx="Filter_node1702900946384",
+filtered_frame = Filter.apply(
+    frame=s3_source_frame,
+    f=lambda row: (row["shareWithResearchAsOfDate"] is not None),
+    transformation_ctx="filtered_frame",
 )
 
 # Script generated for node Customer Trusted
-CustomerTrusted_node1702899055771 = glueContext.write_dynamic_frame.from_options(
-    frame=Filter_node1702900946384,
+glueContext.write_dynamic_frame.from_options(
+    frame=filtered_frame,
     connection_type="s3",
     format="json",
     connection_options={
         "path": "s3://lake-house-stedi/customer_landing/trusted/",
         "partitionKeys": [],
     },
-    transformation_ctx="CustomerTrusted_node1702899055771",
+    transformation_ctx="customer_trusted_frame",
 )
 
+# Commit the job
 job.commit()
+
