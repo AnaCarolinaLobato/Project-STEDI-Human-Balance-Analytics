6e1a73025f43e1f803687ea6cce06c5cd22d2a0b
Ana Lobato <93808443+AnaCarolinaLobato@users.noreply.github.com>
2023-12-22 21:25:24 +1300

    Update customers_curated.py

--- customers_curated.py
+++ customers_curated.py
@@ -4,6 +4,9 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+import re
+from pyspark.sql import functions as SqlFuncs
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -12,46 +15,69 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node accelerometer_landing
-accelerometer_landing_node1703061474056 = glueContext.create_dynamic_frame.from_catalog(
-    database="stedi",
-    table_name="accelerometer_landing",
-    transformation_ctx="accelerometer_landing_node1703061474056",
-)
-
 # Script generated for node customer_trusted
-customer_trusted_node1703061620875 = glueContext.create_dynamic_frame.from_catalog(
-    database="stedi",
-    table_name="customer_trusted",
-    transformation_ctx="customer_trusted_node1703061620875",
-)
-
-# Script generated for node customer privacy join
-customerprivacyjoin_node1703061569715 = Join.apply(
-    frame1=accelerometer_landing_node1703061474056,
-    frame2=customer_trusted_node1703061620875,
-    keys1=["user"],
-    keys2=["email"],
-    transformation_ctx="customerprivacyjoin_node1703061569715",
-)
-
-# Script generated for node Drop Fields
-DropFields_node1703061893643 = DropFields.apply(
-    frame=customerprivacyjoin_node1703061569715,
-    paths=["z", "y", "user", "x", "timestamp"],
-    transformation_ctx="DropFields_node1703061893643",
-)
-
-# Script generated for node Amazon S3
-AmazonS3_node1703062189919 = glueContext.write_dynamic_frame.from_options(
-    frame=DropFields_node1703061893643,
+customer_trusted_node1703212229238 = glueContext.create_dynamic_frame.from_options(
+    format_options={"multiline": False},
     connection_type="s3",
     format="json",
     connection_options={
-        "path": "s3://lake-house-stedi/accelerometer_landing/curated/",
-        "partitionKeys": [],
+        "paths": ["s3://lake-house-stedi/customer_landing/trusted/"],
+        "recurse": True,
     },
-    transformation_ctx="AmazonS3_node1703062189919",
+    transformation_ctx="customer_trusted_node1703212229238",
 )
 
+# Script generated for node acceleromenter_trusted
+acceleromenter_trusted_node1703212227831 = (
+    glueContext.create_dynamic_frame.from_catalog(
+        database="stedi",
+        table_name="accelerometer_trusted",
+        transformation_ctx="acceleromenter_trusted_node1703212227831",
+    )
+)
+
+# Script generated for node Join
+Join_node1703212678990 = Join.apply(
+    frame1=acceleromenter_trusted_node1703212227831,
+    frame2=customer_trusted_node1703212229238,
+    keys1=["user"],
+    keys2=["email"],
+    transformation_ctx="Join_node1703212678990",
+)
+
+# Script generated for node Filter
+Filter_node1703212905731 = Filter.apply(
+    frame=Join_node1703212678990,
+    f=lambda row: (not (row["shareWithResearchAsOfDate"] == 0)),
+    transformation_ctx="Filter_node1703212905731",
+)
+
+# Script generated for node Drop Fields
+DropFields_node1703212984680 = DropFields.apply(
+    frame=Filter_node1703212905731,
+    paths=["z", "user", "y", "x", "timestamp"],
+    transformation_ctx="DropFields_node1703212984680",
+)
+
+# Script generated for node Drop Duplicates
+DropDuplicates_node1703217771523 = DynamicFrame.fromDF(
+    DropFields_node1703212984680.toDF().dropDuplicates(),
+    glueContext,
+    "DropDuplicates_node1703217771523",
+)
+
+# Script generated for node Amazon S3
+AmazonS3_node1703214061895 = glueContext.getSink(
+    path="s3://lake-house-stedi/customer_landing/curated/",
+    connection_type="s3",
+    updateBehavior="UPDATE_IN_DATABASE",
+    partitionKeys=[],
+    enableUpdateCatalog=True,
+    transformation_ctx="AmazonS3_node1703214061895",
+)
+AmazonS3_node1703214061895.setCatalogInfo(
+    catalogDatabase="stedi", catalogTableName="customer_curated"
+)
+AmazonS3_node1703214061895.setFormat("json")
+AmazonS3_node1703214061895.writeFrame(DropDuplicates_node1703217771523)
 job.commit()
